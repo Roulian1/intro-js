85d4f7d56e635d3f012d42d6b2009efa92a7b184
Julien Elinckx <julienelinckx@hotmail.com>
2022-07-14 15:19:23 +0200

    1.2.

--- 1.0.js
+++ 1.0.js
@@ -1,2 +1,8 @@
-let name = "Julien";
-console.log("My name is " + name+"!");
+let r = 10;
+let m = 8;
+
+console.log(r+m);
+console.log(r-m);
+console.log(r/m);
+console.log(r*m);
+console.log(r%m);
---
+++ 1.2.js
@@ -0,0 +1,5 @@
+let name = "Elinckx";
+let firstname = "Julien ";
+let city = "Charleroi";
+
+console.log("My name is "+ firstname + name + " and I live in " + city);
