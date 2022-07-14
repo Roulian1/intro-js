b1942f949956d700f3a5d8f2ff799b0c9487a85e
Julien Elinckx <julienelinckx@hotmail.com>
2022-07-14 15:47:06 +0200

    2.2.

---
+++ 2.1.js
@@ -0,0 +1,7 @@
+let result = prompt("What's your age ?")
+
+if (result >=18) {
+    console.log("You're an adult!");
+} else {
+    console.log("You're a child!");
+}
---
+++ 2.2.js
@@ -0,0 +1,9 @@
+let min = prompt("Give a small number.")
+let max = prompt("Give a big number.")
+let current = prompt("Give a number between your previous numbers.")
+
+if ((current > min)&&(current < max)) {
+    console.log("True")
+} else {
+    console.log("I'm kinda sad for you")
+}
