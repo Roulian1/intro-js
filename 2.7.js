24ccc39a2f344c80ebd88bcb342af8ed7c4dfa8c
Julien Elinckx <julienelinckx@hotmail.com>
2022-07-15 12:27:33 +0200

    3.1

---
+++ 2.7.js
@@ -0,0 +1,10 @@
+let n = Number(prompt("Give a number please."));
+let i = 0;
+let a = 0;
+
+while (n > i) {
+    a += Number(prompt("Give a number."));
+    i++
+}
+
+console.log(a);
---
+++ 3.0.js
@@ -0,0 +1,5 @@
+let arr = ["Guillaume", "Rinaldo", "Benoît", "Xavier.B", "Xavier.C", "Daniela", "Anthony", "Vincent", "Julien.E", "Lucas", "Sophie", "Axel", "Hazem", "Kamilla", "Stéphanie", "Julien.S", "Doriano", "Eddy", "Selim", "Titouan"];
+
+for (let elem of arr) {
+  console.log("In my formation, there's " + elem + "!");
+}
